0f220fabc2f1056a2b4100626f28e8dfdc579632
Ivan Nemets <one.nevan@gmail.com>
2023-10-05 00:00:14 +0300

    add phpunit

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,12 +8,4 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    /**
-     * Testing Psalm in CI.
-     */
-    public function dummyMethod(): bool
-    {
-        return 42;
-    }
 }
